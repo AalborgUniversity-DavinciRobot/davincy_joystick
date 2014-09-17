046656f936d0a3a514b8145b368d25edfb4ca6cf
Erwin <erwin@es.aau.dk>
2014-09-17 15:30:01 +0200

    positions and currents can be read

--- src/joystick_measurement.cpp
+++ src/joystick_measurement.cpp
@@ -10,13 +10,16 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-
 // Namespaces
 using namespace std;
 
 // Defined constants
 #define PORT "/dev/ttyUSB0"			// connected port
 #define BAUDRATE B230400			// Baud rate = 230400 bits/s
+#define GEAR_RATIO_1 5.1			// Gear ratio motor 1
+#define GEAR_RATIO_2 19.0			// Gear ratio motor 2
+#define GEAR_RATIO_3 4.4			// Gear ratio motor 3
+#define GEAR_RATIO_4 19.0			// Gear ratio motor 4
 
 
 // Functions used
@@ -25,7 +28,7 @@ int configure_port(int port);						// Configures the port, sets baudrate, charac
 vector<double> process_message(int i,vector<uint8_t> message);	// Returns 4 Currents and 4 Positions
 double hex_to_dec(uint8_t MSB, uint8_t LSB);		// Converts the hex values into decimal numbers
 vector<double> initialize_position(int port, vector<uint8_t> buffer);
-
+double delta_position(double Theta_old,double Theta_new);
 
 int main(int argc, char **argv)
 {
@@ -33,16 +36,16 @@ int main(int argc, char **argv)
 
     ros::NodeHandle n("~");
     //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-    ros::Rate rate(10);
+    ros::Rate rate(1000);
 
-    /*int i1 = 0;
+    int i1 = 0;
     int i2 = 0;
     int i3 = 0;
     int i4 = 0;
     n.getParam("i1",i1);
     n.getParam("i2",i2);
     n.getParam("i3",i3);
-    n.getParam("i4",i4);*/
+    n.getParam("i4",i4);
 
 
     int port = open_port();
@@ -50,23 +53,30 @@ int main(int argc, char **argv)
     vector<uint8_t> buffer;
     buffer.clear();
 
-    vector<double> init_pos;
-
-    init_pos=initialize_position(port,buffer);
+    vector<double> data_init;
+    data_init=initialize_position(port,buffer);
+    vector<double> data_old = data_init;
+    vector<double> data;
+    double Theta1 = data_init[1];
+    double Theta2 = data_init[3];
+    double Theta3 = data_init[5];
+    double Theta4 = data_init[7];
+    double dTheta1 = 0.0;
+    double dTheta2 = 0.0;
+    double dTheta3 = 0.0;
+    double dTheta4 = 0.0;
 
     while (ros::ok()) // Keep spinning loop until user presses Ctrl+C
     {
 
         int bytes = 0;
-        vector<double> measurements;
+        uint8_t this_byte;
         buffer.clear();
         tcflush(port,TCIFLUSH);
         usleep(5000);	// 5 milliseconds  (18Bytes @ 1kHz -> 5*18=90 bytes per 5 ms)
 
         ioctl(port, FIONREAD, &bytes);	// get number of bytes available at port and store in bytes
         //printf("Size port flushed: %d\n", bytes);
-
-    	uint8_t this_byte;
 
     	if (bytes>37)
     	{
@@ -75,26 +85,29 @@ int main(int argc, char **argv)
 				read(port,&this_byte,1);
 				buffer.push_back(this_byte);
 			}
-
 			for (int i=0;i<buffer.size();i++)
 			{
 				if(buffer[i]==255 && buffer[i+1]==0 && buffer[i+2]<16 && buffer[i+18]==255 && buffer[i+19]==0)
 				{
-					//printf("found start byte\n");
-					measurements = process_message(i+1, buffer);
+					data = process_message(i+1, buffer);
+					dTheta1 = delta_position(data_old[1],data[1]);
+					dTheta2 = delta_position(data_old[3],data[3]);
+					dTheta3 = delta_position(data_old[5],data[5]);
+					dTheta4 = delta_position(data_old[7],data[7]);
+					Theta1 += dTheta1;
+					Theta2 += dTheta2;
+					Theta3 += dTheta3;
+					Theta4 += dTheta4;
 					break;
 				}
-
 			}
-
     	}
+    	printf("%1.4lf\t%1.4lf\t%1.4lf\t%1.4lf\t\n",data[0],data[2],data[4],data[6]);
+    	printf("%4.2lf\t%4.2lf\t%4.2lf\t%4.2lf\t\n",(Theta1-data_init[1])/GEAR_RATIO_1,(Theta2-data_init[3])/GEAR_RATIO_2,(Theta3-data_init[5])/GEAR_RATIO_3,(Theta4-data_init[7])/GEAR_RATIO_4);
+    	data_old = data;
+    	printf("\n");
 
-    	/*for (int i=0;i<measurements.size();i++)
-    	{
-    		printf("%4.0lf\t",measurements[i]);
-    	}printf("\n");*/
-
-    	//uint8_t send_bytes[]={0x00, 0x00, i1, 0x00, i2, 0x00, i3, 0x00, i4, 0xFF};
+    	uint8_t send_bytes[]={0x00, 0x00, i1, 0x00, i2, 0x00, i3, 0x00, i4, 0xFF};
     		/* bytes send {Byte_1, Byte_2, Byte_3, Byte_4, Byte_5, Byte_6, Byte_7, Byte_8, Byte_9, Byte_10}
     		 * Byte_1: Start byte 0x00 (0b00000000)
     		 * Byte_2: Sign of Current motor 1; MSB = 0 Clamp, MSB = 1 Open
@@ -107,8 +120,8 @@ int main(int argc, char **argv)
     		 * Byte_9: Amplitude of Current motor 4
     		 * Byte_10: Stop byte 0xFF (0b11111111)
     		 */
-    		//tcflush(port,TCOFLUSH);	// Flush Output buffer before sending new data
-    		//write(port,send_bytes,10);
+    		tcflush(port,TCOFLUSH);	// Flush Output buffer before sending new data
+    		write(port,send_bytes,10);
 
         //ros::spinOnce(); // Need to call this function often to allow ROS to process incoming messages
 
@@ -117,7 +130,7 @@ int main(int argc, char **argv)
     }
     uint8_t send_bytes[]={0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF};
     tcflush(port,TCOFLUSH);
-    //write(port,send_bytes,10);
+    write(port,send_bytes,10);
     return 0;
 }
 
@@ -151,6 +164,7 @@ int open_port(void)
 
 	return(port);
 }
+
 int configure_port(int port)
 {
 	/* Functions that configures the serial port.
@@ -190,14 +204,16 @@ vector<double> process_message(int i,vector<uint8_t> message)
 	 * which can be interpreted in a better way
 	 *
 	 */
+	double bit_to_I = 5.0/4095.0; //Bit to Current mapping
+
 	vector<double> measurements;
-	measurements.push_back(hex_to_dec(message[i+1],message[i+2]));
+	measurements.push_back(hex_to_dec(message[i+1],message[i+2])*bit_to_I);
 	measurements.push_back(hex_to_dec(message[i+3],message[i+4])*360/2048);
-	measurements.push_back(hex_to_dec(message[i+5],message[i+6]));
+	measurements.push_back(hex_to_dec(message[i+5],message[i+6])*bit_to_I);
 	measurements.push_back(hex_to_dec(message[i+7],message[i+8])*360/2048);
-	measurements.push_back(hex_to_dec(message[i+9],message[i+10]));
+	measurements.push_back(hex_to_dec(message[i+9],message[i+10])*bit_to_I);
 	measurements.push_back(hex_to_dec(message[i+11],message[i+12])*360/2048);
-	measurements.push_back(hex_to_dec(message[i+13],message[i+14]));
+	measurements.push_back(hex_to_dec(message[i+13],message[i+14])*bit_to_I);
 	measurements.push_back(hex_to_dec(message[i+15],message[i+16])*360/2048);
 	return measurements;
 }
@@ -274,4 +290,23 @@ vector<double> initialize_position(int port, vector<uint8_t> buffer)
 		}
 	}
 	return measurements;
+}
+
+double delta_position(double Theta_old,double Theta_new)
+{
+	double dTheta;
+
+		if (Theta_old>290.0 && Theta_new<70.0)
+		{
+			dTheta = 360.0-Theta_old+Theta_new;
+		}
+		else if (Theta_old<70.0 && Theta_new>290.0)
+		{
+			dTheta = -Theta_old-(360-Theta_new);
+		}
+		else
+		{
+			dTheta = Theta_new-Theta_old;
+		}
+	return dTheta;
 }
